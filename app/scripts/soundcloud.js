5fa57cf59c448a214fd0e2afeb11c4158436d965
Emory Griffith <emorygriffith91@gmail.com>
2014-12-10 20:28:27 -0500

    added kevins soundcloud

--- app/scripts/soundcloud.js
+++ app/scripts/soundcloud.js
@@ -1,30 +1,18 @@
 (function() {
 
 
+  // initialize client with app credentials
+  SC.initialize({
+    client_id: 'edf7ffe41584a2bd4b4b16da8bfd4bfc',
+    redirect_uri: 'http://localhost:9000/#/login'
+  });
 
-
-  // // initiate auth popup
-  // var SCsignin = function(){
-  //     SC.connect(function() {
-  //       SC.get('/me', function(me) {
-  //         alert('Hello, ' + me.username);
-  //       });
-  //     });
-  // };
-
-
-  function playSomeSound(genre){
-    SC.get("/tracks", {genre: genre}, function(tracks){
-      var random = Math.floor(Math.random() * 49);
-      SC.oEmbed(tracks[random].uri, {auto_play:true}, document.getElementbyID('soundcloud'))
-    }
-
-    App.Views.onload = function(){
-      SC.initialize({
-        client_id: 'edf7ffe41584a2bd4b4b16da8bfd4bfc'
-      });
-
-    };
+  //
+  // function playSomeSound(genre){
+  //   SC.get("/tracks", {genre: genre}, function(tracks){
+  //     var random = Math.floor(Math.random() * 49);
+  //     SC.oEmbed(tracks[random].uri, {auto_play:true}, document.getElementbyID('soundcloud'))
+  //   }
 
 
 
